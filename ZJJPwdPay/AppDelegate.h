882387b9bcafc290178ec94b7453b7c5fa8b5927
张锦江 <zhangjinjiang@zhangjinjiangdeMacBook-Pro.local>
2017-09-23 17:35:33 +0800

    Initial Commit

---
+++ ZJJPwdPay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJJPwdPay
+//
+//  Created by 张锦江 on 2017/9/23.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
